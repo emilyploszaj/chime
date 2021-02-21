2255fe15857ebb16a8cb7418b29136e7fded4d69
emilyploszaj <emilyploszaj@gmail.com>
2021-02-20 20:22:29 -0600

    Fix texture dependency error

--- src/main/java/dev/emi/chime/mixin/JsonUnbakedModelMixin.java
+++ src/main/java/dev/emi/chime/mixin/JsonUnbakedModelMixin.java
@@ -1,15 +1,18 @@
 package dev.emi.chime.mixin;
 
 import java.io.InputStreamReader;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.mojang.datafixers.util.Pair;
 
 import org.apache.logging.log4j.LogManager;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -20,9 +23,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import dev.emi.chime.override.ChimeArmorOverrideLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.model.ModelLoader;
+import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.render.model.json.JsonUnbakedModel;
 import net.minecraft.client.render.model.json.ModelOverride;
 import net.minecraft.client.render.model.json.ModelOverrideList;
+import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.resource.Resource;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
@@ -31,13 +36,33 @@ import net.minecraft.util.Identifier;
 public class JsonUnbakedModelMixin {
 	@Unique
 	private static final Gson GSON = new Gson();
-	@Shadow @Final
+	@Unique
+	private boolean chimeOverrides = false;
+	@Shadow
 	private List<ModelOverride> overrides;
 	@Shadow
 	public String id;
 
 	@Inject(at = @At("HEAD"), method = "compileOverrides")
 	private void compileOverrides(ModelLoader modelLoader, JsonUnbakedModel parent, CallbackInfoReturnable<ModelOverrideList> info) {
+		calculateChimeOverrides();
+	}
+
+	@Inject(at = @At("HEAD"), method = "getModelDependencies")
+	private void getModelDependencies(CallbackInfoReturnable<Collection<Identifier>> info) {
+		calculateChimeOverrides();
+	}
+	
+	@Inject(at = @At("HEAD"), method = "getTextureDependencies")
+	private void getTextureDependencies(Function<Identifier, UnbakedModel> unbakedModelGetter, Set<Pair<String, String>> unresolvedTextureReferences, CallbackInfoReturnable<Collection<SpriteIdentifier>> info) {
+		calculateChimeOverrides();
+	}
+
+	@Unique
+	void calculateChimeOverrides() {
+		if (chimeOverrides) {
+			return;
+		}
 		ModelOverride.Deserializer deserializer = new ChimeArmorOverrideLoader.Deserializer2();
 		ResourceManager manager = MinecraftClient.getInstance().getResourceManager();
 		try {
@@ -64,5 +89,6 @@ public class JsonUnbakedModelMixin {
 		} catch (Exception e) {
 			LogManager.getLogger("chime").warn("[chime] IO error reading item overrides: " + id, e);
 		}
+		chimeOverrides = true;
 	}
 }
